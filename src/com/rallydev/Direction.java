29d14b55b28a2af3d66b78367faeeb626a853c7a
lamchau <lamchau@gmail.com>
2014-03-30 23:56:17 -0700

    remove direction iterator

--- src/com/rallydev/Direction.java
+++ src/com/rallydev/Direction.java
@@ -1,94 +1,14 @@
 package com.rallydev;
 
-import java.util.ListIterator;
-
 /**
  * Relative clockwise movement direction, starting with right as the priority direction.
  */
 public enum Direction {
   RIGHT, DOWN, LEFT, UP;
 
-  /**
-   * Convenience class for traversing relative directions forwards/backwards.
-   */
-  // TODO: remove
-  private static class DirectionIterator implements ListIterator<Direction> {
-    private final Direction[] values = values();
-    private int currentIndex = Direction.UP.ordinal();
-
-    @Override
-    public void add(Direction e) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean hasNext() {
-      return true;
-    }
-
-    @Override
-    public boolean hasPrevious() {
-      return true;
-    }
-
-    @Override
-    public Direction next() {
-      this.currentIndex = nextIndex();
-      return values[this.currentIndex];
-    }
-
-    @Override
-    public int nextIndex() {
-      return getIndex(currentIndex + 1);
-    }
-
-    @Override
-    public Direction previous() {
-      this.currentIndex = previousIndex();
-      return values[this.currentIndex];
-    }
-
-    @Override
-    public int previousIndex() {
-      return getIndex(currentIndex - 1);
-    }
-
-    @Override
-    public void remove() {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void set(Direction e) {
-      if (e == null) {
-        throw new IllegalArgumentException("Direction cannot be null");
-      }
-      this.currentIndex = getIndex(e.ordinal());
-    }
-
-    /**
-     * Alias for {@code Direction#getDirection(int)}.
-     *
-     * @param index the relative index
-     * @return the absolute ordinal index
-     */
-    private int getIndex(int index) {
-      return getDirection(index).ordinal();
-    }
-  }
-
   // cache directions
   private static final Direction[] VALUES = values();
   public static final int LENGTH = VALUES.length;
-
-  /**
-   * Create a circular (continuous) <code>Direction</code> iterator/generator.
-   *
-   * @return a direction iterator
-   */
-  public static ListIterator<Direction> createIterator() {
-    return new DirectionIterator();
-  }
 
   /**
    * Convenience method for getting a relative <code>Direction</code>. <br />
